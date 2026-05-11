61d69abd301ed9475942657ea1a3c387de92dc07
Anonymous Author <anonymous@example.com>
2026-05-11 13:35:05 -0500

    Alogritm Updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = () => {
     const observerOptions = { threshold: 0.1 };
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
-            if (entry.isIntersecting) entry.target.classList.add('animate-up');
+            if (entry.isIntersecting) entry.target.classList.add('visible');
         });
     }, observerOptions);
 
